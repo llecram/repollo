2716ee05062cdd57c76beecd6e16fff109c701c6
Dennis <31170537+llecram@users.noreply.github.com>
2018-05-24 21:46:46 -0500

    Update main.cpp

--- myRSATRUE/main.cpp
+++ myRSATRUE/main.cpp
@@ -2,12 +2,15 @@
 #include "rsa.h"
 #include "operaciones.h"
 using namespace std;
-int main()
-{
-    /*rsa first;
-    cout<<first.getkeypri()<<endl<<first.getkeypub()<<endl<<first.getp()<<endl<<first.getq();
-    first.cifrado("h");*/
-    rsa second(27583,27583,10243,40478167);
-    second.descifrado(673325503);
+int main(){
+    char mensaje[100000];
+	cin.getline(mensaje, 100000);
+	string mesagge(mensaje);
+    rsa first;
+    cout<<first.getn()<<endl<<first.getkeypub()<<endl<<first.getkeypri()<<endl;
+    vector<int> a(1);
+    a={2192 ,14435, 1910 ,0, 17700 ,3524 ,14435 ,10183 ,14435, 17700 ,17880, 15885 ,6813, 0 ,15885};
+    first.descifrado(a);
+    //first.cifrado(mensaje);
     return 0;
 }
