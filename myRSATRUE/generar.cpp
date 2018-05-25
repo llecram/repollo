81597b48fcc707a2d574f36d702d51f59693db15
Dennis <31170537+llecram@users.noreply.github.com>
2018-05-24 21:46:05 -0500

    Update generar.cpp

--- myRSATRUE/generar.cpp
+++ myRSATRUE/generar.cpp
@@ -1,16 +1,16 @@
 #include "generar.h"
 long long generarprimo(){
-    srand(time(NULL));
+   // srand(time(NULL));
 	long long x=rand();
-	bool flag=Primalidad(x,5);
+	bool flag= fermat1(x,5);
 	while(!flag){
 		x=rand();
-		flag=Primalidad(x,5);
+		flag=fermat1(x,5);
 	}
 	return x;
 }
 long long generare(long long n){
-    long long x=rand();
+    long long x=rand()%n;
     bool flag=true;
     while (flag){
         if (x>1 && x<n && euclides(n,x)==1){
