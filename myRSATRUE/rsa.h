2f730a5ab0b8db169dee02e12f4516bbf23a3e4c
Dennis <31170537+llecram@users.noreply.github.com>
2018-05-24 21:48:47 -0500

    Update rsa.h

--- myRSATRUE/rsa.h
+++ myRSATRUE/rsa.h
@@ -4,10 +4,12 @@
 #include<string>
 #include<math.h>
 #include<ctime>
+#include<vector>
 using namespace std;
 static string alphabet = "abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ.,_-0123456789";
 class rsa{
     public:
+        long long e;
         long long p;
         long long q;
         bool flag;
@@ -16,13 +18,13 @@ class rsa{
         long long n;
         long long phieu;
         rsa();
-        rsa(long long, long long ,long long ,long long );
-        long long cifrado(string);
-        string descifrado(long long);
+        rsa(long long, long long );
+        void cifrado(string);
+        void descifrado(vector<int>);
         long long getkeypub()const;
         long long getkeypri()const;
-        long long getp()const;
+        long long getn()const;
         long long getq()const;
 };
-
 #endif // RSA_H
+
